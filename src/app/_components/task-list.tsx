0c01de6135e60d3fedf45e79b471ca2b26229de1
Sam Ievins <sievins8012@gmail.com>
2025-02-16 14:48:48 +0000

    task list handles long labels

--- src/app/_components/task-list.tsx
+++ src/app/_components/task-list.tsx
@@ -11,11 +11,20 @@ interface Item {
 }
 
 const items: Item[] = [
-  { id: "item1", label: "Item 1" },
+  {
+    id: "item1",
+    label:
+      "Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 Item 1 ",
+  },
   { id: "item2", label: "Item 2" },
-  { id: "item3", label: "Item 3" },
+  {
+    id: "item3",
+    label:
+      "Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 Item 3 ",
+  },
   { id: "item4", label: "Item 4" },
   { id: "item5", label: "Item 5" },
+  { id: "item6", label: "Item 6" },
 ];
 
 export function TaskList() {
@@ -36,20 +45,20 @@ export function TaskList() {
         <div key={item.id}>
           <div
             className={cn(
-              "flex items-center rounded-md p-2 transition-colors",
+              "flex w-[32rem] items-center rounded-md p-2 transition-colors",
               highlightedItems.includes(item.id) ? "bg-primary/40" : "",
             )}
           >
             <Checkbox id={item.id} />
             <label
               htmlFor={item.id}
-              className="ml-2 flex-grow text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              className="ml-2 max-w-sm text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               {item.label}
             </label>
             <button
               onClick={() => handleHighlight(item.id)}
-              className="ml-28 text-sm text-primary hover:underline focus:outline-none sm:ml-96"
+              className="ml-auto text-sm text-primary hover:underline focus:outline-none"
             >
               Mark
             </button>
