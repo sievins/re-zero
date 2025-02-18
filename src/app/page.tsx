622d1256457475bf4c1238fd3679c586fab7a376
Sam Ievins <sievins8012@gmail.com>
2025-02-18 19:48:34 +0000

    update gaps between elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,20 @@ export default function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#f7fafd] to-[#ebf2fa]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <div className="container flex flex-col items-center justify-center gap-10 px-4 py-16 sm:gap-16">
           {/* Main content */}
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Re: <span className="text-primary">Zero</span>
-          </h1>
-          <h2 className="text-l font-extrabold tracking-tight sm:text-xl">
-            Complete tasks with zero resistance
-          </h2>
-          <TaskForm />
-          <TaskList />
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+              Re: <span className="text-primary">Zero</span>
+            </h1>
+            <h2 className="text-l font-bold tracking-tight sm:text-xl">
+              Complete tasks with zero resistance
+            </h2>
+          </div>
+          <div className="flex flex-col items-center gap-8">
+            <TaskForm />
+            <TaskList />
+          </div>
           {/* Authentication */}
           <SignedOut>
             <SignInButton />
