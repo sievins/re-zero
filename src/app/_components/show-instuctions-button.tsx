d1ee7a4ad0a4cfbfb817f69325981adcf7835cce
Sam Ievins <sievins8012@gmail.com>
2025-02-24 20:18:32 +0000

    recognise Mark Forster and animate show instructions button

--- src/app/_components/show-instuctions-button.tsx
+++ src/app/_components/show-instuctions-button.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 import { getCookie, setCookie } from "~/lib/actions/cookie";
 import { cookieNames } from "~/lib/actions/cookie-names";
-import { cn } from "~/lib/utils";
 
 export function ShowIntructionsButton() {
+  const [parent] = useAutoAnimate();
+
   const [isShowButtonVisible, setIsShowButtonVisible] = useState(false);
 
   useEffect(() => {
@@ -20,12 +22,18 @@ export function ShowIntructionsButton() {
   });
 
   return (
-    <Button
-      variant="outline"
-      className={cn(!isShowButtonVisible && "hidden")}
-      onClick={() => setCookie(cookieNames.showInstructions, "true")}
-    >
-      Show instructions
-    </Button>
+    // "min-w-40" required to prevent annimation jumping around when inside flex
+    // https://github.com/formkit/auto-animate/issues/182#issuecomment-2358393967
+    <div ref={parent} className="min-w-40">
+      {isShowButtonVisible && (
+        <Button
+          variant="outline"
+          onClick={() => setCookie(cookieNames.showInstructions, "true")}
+          className="min-w-40"
+        >
+          Show instructions
+        </Button>
+      )}
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ import {
   SignedIn,
 } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
-import { LogIn, LogOut } from "lucide-react";
-import { Button } from "~/components/ui/button";
+import Link from "next/link";
+import { ExternalLink, LogIn, LogOut } from "lucide-react";
+import { Button, buttonVariants } from "~/components/ui/button";
 import { HydrateClient, api } from "~/trpc/server";
 import { Instructions } from "~/app/_components/instructions";
 import { TaskList } from "~/app/_components/task-list";
 import { TaskForm } from "~/app/_components/task-form";
 import { ShowIntructionsButton } from "~/app/_components/show-instuctions-button";
+import { cn } from "~/lib/utils";
 
 export default async function Home() {
   const user = await currentUser();
@@ -20,26 +22,41 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#f7fafd] to-[#ebf2fa]">
-        {/* Authentication */}
-        <div className="my-4 mr-8 flex w-screen justify-end space-x-2">
-          <ShowIntructionsButton />
-          <SignedOut>
-            <SignInButton>
-              <Button>
-                <LogIn /> Sign in
-              </Button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <SignOutButton>
-              <Button variant="outline">
-                <LogOut /> Sign out
-              </Button>
-            </SignOutButton>
-          </SignedIn>
+        {/* Buttons in header */}
+        <div className="my-4 flex w-screen justify-end px-4 sm:justify-between">
+          {/* Buttons on left */}
+          <Link
+            className={cn(
+              buttonVariants({ variant: "link" }),
+              "hidden px-0 sm:flex",
+            )}
+            href="http://markforster.squarespace.com/blog/2022/6/14/resistance-how-to-make-the-most-of-it-the-resistance-zero-sy.html"
+            target="_blank"
+          >
+            System designed by Mark Forster <ExternalLink />
+          </Link>
+          {/* Buttons on right */}
+          <div className="flex space-x-4">
+            <ShowIntructionsButton />
+            {/* Authentication */}
+            <SignedOut>
+              <SignInButton>
+                <Button>
+                  <LogIn /> Sign in
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <SignOutButton>
+                <Button variant="outline">
+                  <LogOut /> Sign out
+                </Button>
+              </SignOutButton>
+            </SignedIn>
+          </div>
         </div>
+        {/* Main content */}
         <div className="container flex flex-col items-center justify-center gap-10 px-4 pb-16 pt-4 sm:gap-16 sm:pt-16">
-          {/* Main content */}
           <div className="flex flex-col items-center gap-4">
             <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
               Re: <span className="text-primary">Zero</span>
