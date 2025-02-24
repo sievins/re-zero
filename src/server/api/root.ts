599ba5bfa902d42d9d145da8480df31b6ac61878
Sam Ievins <sievins8012@gmail.com>
2025-02-24 15:52:54 +0000

    remove post code from t3 template

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { taskRouter } from "~/server/api/routers/task";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
@@ -8,7 +7,6 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   task: taskRouter,
 });
 
